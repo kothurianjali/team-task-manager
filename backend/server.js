5e7c8a32d7d7feecec99c99d32d063056e522d66
kothurianjali <kothurianjali@gmail.com>
2026-05-10 13:55:21 +0530

    changes comitted

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+
 require('./database');
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
-app.listen(5000, () => {
-console.log('Server Running on Port 5000');
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Backend Server Running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server Running on Port ${PORT}`);
 });
